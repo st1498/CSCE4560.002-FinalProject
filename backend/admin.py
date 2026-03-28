e2e741c5e5ca4d3a037809229dce8a66ccf008c6
kudzosteve <kudzosteve24726@gmail.com>
2026-03-28 10:00:17 -0500

    Delete backend directory

--- backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Product, Customer, Order, Subscription
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Subscription)
