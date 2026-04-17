ba932238d7ceec5bdf24d3dd5a3c6ea1979492a2
kudzosteve <kudzosteve24726@gmail.com>
2026-04-17 13:21:11 -0500

    Correct MySQL table definitions

--- create_tables.sql
+++ create_tables.sql
@@ -1,39 +1,43 @@
-CREATE TABLE 'Products' (
-    'id' INTEGER AUTOINCREMENT,
-    'name' VARCHAR(100) NOT NULL,
-    'description' TEXT,
-    'price' DECIMAL(10, 2) NOT NULL,
+CREATE TABLE Products (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE 'Customers' (
-    'id' INTEGER AUTOINCREMENT,
-    'first_name' VARCHAR(20) NOT NULL,
-    'last_name' VARCHAR(20) NOT NULL,
-    'email' VARCHAR(50) NOT NULL UNIQUE,
-    'username' VARCHAR(20) NOT NULL UNIQUE,
-    'password' VARCHAR(255) NOT NULL,
-    'created_at' DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE Customers (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password_hash VARCHAR(250) NOT NULL,
+    date_registered DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE 'Orders' (
-    'id' INTEGER AUTOINCREMENT,
-    'num_items' INTEGER NOT NULL,
-    'total_cost' DECIMAL(10, 2) NOT NULL,
-    'order_date' DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    'order_status' ENUM('pending', 'sold') NOT NULL DEFAULT 'pending',
+CREATE TABLE Orders (
+    id INT AUTO_INCREMENT,
+    num_items INT NOT NULL,
+    total_cost DECIMAL(10, 2) NOT NULL,
+    order_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('pending', 'sold') NOT NULL DEFAULT 'pending',
+    product_id INT NOT NULL,
+    customer_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (product_id) REFERENCES Products(id),
     FOREIGN KEY (customer_id) REFERENCES Customers(id)
-)
+);
 
-CREATE TABLE 'Subscriptions' (
-    'id' INTEGER AUTOINCREMENT,
-    'license_key' VARCHAR(20) NOT NULL UNIQUE,
-    'start_date' DATETIME NOT NULL,
-    'end_date' DATETIME,
+CREATE TABLE Subscriptions (
+    id INT AUTO_INCREMENT,
+    license_key VARCHAR(20) NOT NULL UNIQUE,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME,
+    product_id INT NOT NULL,
+    customer_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (product_id) REFERENCES Products(id),
     FOREIGN KEY (customer_id) REFERENCES Customers(id)
-)
+);
