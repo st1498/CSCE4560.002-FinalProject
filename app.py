c2d6400a6cbdd191a41527076d6e6d5f0fc46ba2
ND7871 <NateDenton@my.unt.edu>
2026-04-30 01:53:32 +0000

    Remove duplicate Google login route

--- app.py
+++ app.py
@@ -150,7 +150,6 @@ def validate_email(email) -> bool:
 
 @app.route('/login/google')
 @limiter.limit("10 per minute")
-@app.route('/login/google')
 def google_login():
     redirect_uri = url_for('google_authorize', _external=True, _scheme='https')
     return google.authorize_redirect(redirect_uri)
