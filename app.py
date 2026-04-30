1c120d659717af3f08a3cb4cc746fd3aa364bd68
ND7871 <NateDenton@my.unt.edu>
2026-04-30 02:06:18 +0000

    Update user info retrieval method in Google login

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def google_login():
 def google_authorize():
     try:
         token = google.authorize_access_token()
-        user_info = google.parse_id_token(token)
+        user_info = google.get('userinfo').json()
 
         if not user_info:
             flash('Google login failed.', 'error')
